d39a31b118f498f12db47725e1aff5feaa03ea7c
Yi Cao <me@ycao.top>
2023-08-13 20:30:13 +0800

    Initial Commit

---
+++ SensorLogger/SensorLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SensorLoggerApp.swift
+//  SensorLogger
+//
+//  Created by Kerman on 2023/8/13.
+//
+
+import SwiftUI
+
+@main
+struct SensorLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
