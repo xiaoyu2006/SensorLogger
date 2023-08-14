e749dba7ec3b3acb3b8a9b9a74a0d0e5f481e21c
Yi Cao <me@ycao.top>
2023-08-14 17:51:03 +0800

    All in one calibrated logger

--- SensorLogger/ContentView.swift
+++ SensorLogger/ContentView.swift
@@ -13,8 +13,42 @@ struct ContentView: View {
     @State var accelerometersTimer: Timer? = nil
     @State var gyrosTimer: Timer? = nil
     @State var magnetometersTimer: Timer? = nil
+    @State var deviceMotionTimer: Timer? = nil
     
     let LOGGING_INTERVAL = 1.0 / 50.0
+    
+    func toggleDeviceMotion() {
+        if self.deviceMotionTimer != nil {
+            // Stop it
+            self.deviceMotionTimer!.invalidate()
+            self.deviceMotionTimer = nil
+            self.motion.stopDeviceMotionUpdates()
+            return
+        }
+        
+        // Start it
+        self.motion.startDeviceMotionUpdates()
+        
+        let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            .appendingPathComponent("\(Date().timeIntervalSince1970)_deviceMotion")
+            .appendingPathExtension("csv")
+        
+        if !FileManager.default.createFile(atPath: url.path, contents: nil) {
+            print("File not created????")
+        }
+        let fileHandle = FileHandle(forWritingAtPath: url.path)
+        fileHandle!.write("timestamp,altroll,altpitch,altyaw,rotx,roty,rotz,gravx,gravy,gravz,usracclx,usraccly,usracclz\n".data(using: .utf8)!)
+
+        self.deviceMotionTimer = Timer(fire: Date(), interval: LOGGING_INTERVAL, repeats: true) { (timer) in
+
+            if let motionData = self.motion.deviceMotion {
+                fileHandle!.write("\(Date().timeIntervalSince1970),\(motionData.attitude.roll),\(motionData.attitude.pitch),\(motionData.attitude.yaw),\(motionData.rotationRate.x),\(motionData.rotationRate.y),\(motionData.rotationRate.z),\(motionData.gravity.x),\(motionData.gravity.y),\(motionData.gravity.z),\(motionData.userAcceleration.x),\(motionData.userAcceleration.y),\(motionData.userAcceleration.z)\n".data(using: .utf8)!)
+            }
+        }
+        RunLoop.current.add(self.deviceMotionTimer!, forMode: .default)
+    }
+    
+    
     
     func toggleMagnometers() {
         if self.magnetometersTimer != nil {
@@ -127,6 +161,13 @@ struct ContentView: View {
                 Text("Start Logging Magnometers") :
                 Text("Stop Logging Magnometers")
             }
+            Button(action: toggleDeviceMotion) {
+                (self.deviceMotionTimer == nil) ?
+                Text("Start Logging Device Motion") :
+                Text("Stop Logging Device Motion")
+            }
+            Text("Note: Data are retreved from Apple's sensors, acceleration is in g, rotation rate is in rad/s, magnetic field is in microteslas.")
+            
             // TODO: Add other sensors
         }
         .padding()
@@ -135,6 +176,7 @@ struct ContentView: View {
             self.motion.accelerometerUpdateInterval = LOGGING_INTERVAL
             self.motion.gyroUpdateInterval = LOGGING_INTERVAL
             self.motion.magnetometerUpdateInterval = LOGGING_INTERVAL
+            self.motion.deviceMotionUpdateInterval = LOGGING_INTERVAL
         }
     }
 }
